ff4bfff7e5aa2cf69dd00f3637304aa45c22d1a2
satinath-nit <32492604+satinath-nit@users.noreply.github.com>
2021-07-20 09:50:51 -0500

    Initial Version

---
+++ src/main/java/com/example/demo/DemoCircleciApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCircleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCircleciApplication.class, args);
+	}
+
+}
